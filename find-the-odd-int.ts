b7cc7f687e586bf8ac7189a188d933c9cd999576
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-14 11:11:54 +0100

    feat(find-the-odd-int): map number of occurences

--- find-the-odd-int.ts
+++ find-the-odd-int.ts
@@ -1,3 +1,15 @@
-const findTheOddInt = (arr: number[]) => {};
+const findTheOddInt = (arr: number[]) => {
+  // count number of occurences for each unqiue item in array
+  const numberOfOccurences = {} as Record<number, number>;
 
-findTheOddInt([7]);
+  arr.map((num) => {
+    numberOfOccurences[num] =
+      typeof numberOfOccurences[num] !== "undefined"
+        ? numberOfOccurences[num] + 1
+        : 1;
+  });
+
+  console.log(numberOfOccurences);
+};
+
+console.log(findTheOddInt([7]));
