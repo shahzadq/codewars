d687e7b2ccabb0da71702d7d561ac917899958a2
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-14 11:18:53 +0100

    feat(find-the-odd-int): add all tests and return first item

--- find-the-odd-int.ts
+++ find-the-odd-int.ts
@@ -1,3 +1,5 @@
+const isDefined = <T>(tbd?: T): tbd is T => typeof tbd !== "undefined";
+
 const findTheOddInt = (arr: number[]) => {
   // count number of occurences for each unqiue item in array
   const numberOfOccurences = {} as Record<number, number>;
@@ -9,10 +11,20 @@ const findTheOddInt = (arr: number[]) => {
         : 1;
   });
 
-  const i = Object.keys(numberOfOccurences).map((key) => {
-    const value = numberOfOccurences[key];
-    if (value % 2) return key;
-  });
+  return Object.keys(numberOfOccurences)
+    .map((key) => {
+      const value = numberOfOccurences[key];
+      if (value % 2) return key;
+    })
+    .filter(isDefined)[0];
 };
 
-console.log(findTheOddInt([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+const tests = [
+  [7],
+  [0],
+  [1, 1, 2],
+  [0, 1, 0, 1, 0],
+  [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1],
+];
+
+tests.map((test) => console.log(findTheOddInt(test)));
