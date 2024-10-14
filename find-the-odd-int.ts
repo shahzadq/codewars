8174cfb3c488651a47d7fcce5fda5808b589b52e
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-10-14 11:26:58 +0100

    feat(find-the-odd-int): extract array count number of occurences

--- find-the-odd-int.ts
+++ find-the-odd-int.ts
@@ -1,15 +1,16 @@
 const isDefined = <T>(tbd?: T): tbd is T => typeof tbd !== "undefined";
 
+const arrayCountNumberOfOccurences = <T extends string | number | symbol>(
+  arr: T[]
+) =>
+  arr.reduce((accumulator, current) => {
+    accumulator[current] = (accumulator[current] || 0) + 1;
+    return accumulator;
+  }, {} as Record<T, number>);
+
 const findTheOddInt = (arr: number[]) => {
   // count number of occurences for each unqiue item in array
-  const numberOfOccurences = {} as Record<number, number>;
-
-  arr.map((num) => {
-    numberOfOccurences[num] =
-      typeof numberOfOccurences[num] !== "undefined"
-        ? numberOfOccurences[num] + 1
-        : 1;
-  });
+  const numberOfOccurences = arrayCountNumberOfOccurences(arr);
 
   return Object.keys(numberOfOccurences)
     .map((key) => {
@@ -27,4 +28,6 @@ const tests = [
   [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1],
 ];
 
-tests.map((test) => console.log(findTheOddInt(test)));
+tests.map((test) =>
+  console.log({ [JSON.stringify(test)]: findTheOddInt(test) })
+);
